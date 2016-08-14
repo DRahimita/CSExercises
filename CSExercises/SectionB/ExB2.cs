7568c7db27133fc7929bce0220f4450d952595ee
DRahimita <e0046830@u.nus.edu>
2016-08-14 21:33:37 +0800

    Update ExB2.cs

--- CSExercises/SectionB/ExB2.cs
+++ CSExercises/SectionB/ExB2.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +12,7 @@ namespace CSExercises
     //The square root should be rounded to 3 decimal places.
     //Input	    Output
     //0	        0.000
-    //25	    5
+    //25	    5       --->is this supposed to be 5.000?(3 decimal places)
     //3	        1.732
     //300	    17.321
 
@@ -21,14 +21,23 @@ namespace CSExercises
         public static void Main(string[] args)
         {
             //YOUR CODE HERE
+            Console.WriteLine("Key in any number, please");
+            double entryNumber = Convert.ToDouble(Console.ReadLine());
+            double resultNumber = System.Math.Sqrt(entryNumber);
+            Console.WriteLine("Square root of {0} is {1:##0.000}", entryNumber, resultNumber);
+            Console.ReadLine();
         }
 
-        public static string SQRT(double x)
+        /*Sorry, I don't get the below part yet. Would you mind to tell us more. The above code works, but I would like to know 
+        how to utilize the code block below*/
+         
+        /*public static string SQRT(double x)
         {
             //YOUR CODE HERE: return the square root of x and then use String.Format 
             //to format it. The format is similar to WriteLine
             return null;
 
-        }
+        }*/
+
     }
 }
