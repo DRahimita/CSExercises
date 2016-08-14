5a484fd5213f17164b8bdc7768567e75c955ec07
DRahimita <e0046830@u.nus.edu>
2016-08-14 21:03:25 +0800

    Update ExA3.cs

--- CSExercises/SectionA/ExA3.cs
+++ CSExercises/SectionA/ExA3.cs
@@ -1,32 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CSExercises
+namespace exerciseA3
 {
-    //Write a program that will takes an integer as input and prints the square of that number
-    //For this exercise, the input and output has been implemented for you in the template
-    //You need to implement the square function
-
-    public class ExA3
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            string inputString = Console.ReadLine();
-            int inputInt = Convert.ToInt32(inputString);
-
-            int result = square(inputInt);
-
-            Console.WriteLine(result);
+            Console.WriteLine("Key in an integer number");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int squareNumber = Number * Number;
+            Console.WriteLine("{0} squared is {1}", Number, squareNumber);
+            Console.ReadLine();
         }
+        /*Hi Hans. I don't understand the code block below and the logic behind it. I have the same
+         difficulties in Exercise A4. How do we use two Main() methods here?Is that allowed?*/
 
-        public static int square(int x)
+        /*public static int square(int x)
         {
             //PUT YOUR CODE HERE
             return 0;
 
-        }
+        }*/
     }
 }
+
