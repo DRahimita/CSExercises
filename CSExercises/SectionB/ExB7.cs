c3d934699fb98887118c321f715889976c8917ed
DRahimita <e0046830@u.nus.edu>
2016-08-15 00:17:24 +0800

    Update ExB7.cs
    I wrote the exact same code before, yet the result was different. I ended up writing everything back from the scratch. Do you what might be the cause?

--- CSExercises/SectionB/ExB7.cs
+++ CSExercises/SectionB/ExB7.cs
@@ -19,13 +19,22 @@ namespace CSExercises
         public static void Main(string[] args)
         {
             //YOUR CODE HERE
+            Console.Write("How far have you traveled(in km)?");
+            double input = Convert.ToDouble(Console.ReadLine());
+            double output = 2.4 + input * 0.4;
+            Console.WriteLine("Your taxi fare is ${0}",output);
+            Console.ReadLine();
+        
         }
+         /*Hi Hans, I don't understand the logic of the part below. While the code above works, I would like to know how to do it with
+        two different Main() methods (or 'code of blocks??' Sorry, I'm still catching up on the jargons)*/
 
-        public static double CalculateFare(double distance)
+
+        /*public static double CalculateFare(double distance)
         {
             //YOUR CODE HERE
             return 0;
 
-        }
+        }*/
     }
 }
