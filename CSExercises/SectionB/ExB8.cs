fbc002d38ddd3942ad851f9bc1f6a065862c42e9
DRahimita <e0046830@u.nus.edu>
2016-08-15 00:47:33 +0800

    Update ExB8.cs
    How do you round it to two decimal places?
    e.g: I want the output to show it as 3.70 instead of 3.7
    
    I tried putting the Round and ###.00 but it shows up as 4.00 and 3.72

--- CSExercises/SectionB/ExB8.cs
+++ CSExercises/SectionB/ExB8.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,13 +16,22 @@ namespace CSExercises
         public static void Main(string[] args)
         {
             //YOUR CODE HERE
+            Console.Write("How far have you traveled(in km)?");
+            double input = Convert.ToDouble(Console.ReadLine());
+            double output = 2.4 + input * 0.4;
+            Console.WriteLine("Your taxi fare is ${0:###.#}", output);
+            Console.ReadLine();
         }
 
-        public static double CalculateFare(double distance)
+        /*Hi Hans, I don't understand the logic of the part below. While the code above works, I would like to know how to do it with
+          two different Main() methods (or 'code of blocks??' Sorry, I'm still catching up on the jargons)*/
+
+
+        /*public static double CalculateFare(double distance)
         {
             //YOUR CODE HERE
             return 0;
 
-        }
+        }*/
     }
 }
