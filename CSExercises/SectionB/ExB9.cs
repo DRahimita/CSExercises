14ace29f395c518083e8262ec7462a4f5da608f8
DRahimita <e0046830@u.nus.edu>
2016-08-15 01:10:15 +0800

    Update ExB9.cs

--- CSExercises/SectionB/ExB9.cs
+++ CSExercises/SectionB/ExB9.cs
@@ -17,13 +17,23 @@ namespace CSExercises
         public static void Main(string[] args)
         {
             //YOUR CODE HERE
+            Console.Write("How far have you traveled(in km)?");
+            double input = Convert.ToDouble(Console.ReadLine());
+            double fareTimesTen = (2.4 + input * 0.4)*10;
+            double output = (System.Math.Ceiling(fareTimesTen))/10;
+            Console.WriteLine("Your taxi fare is ${0:###.00}", output);
+            Console.ReadLine();
         }
 
-        public static double CalculateFare(double distance)
+        /*Hi Hans, I don't understand the logic of the part below. While the code above works, I would like to know how to do it with
+        two different Main() methods (or 'code of blocks??' Sorry, I'm still catching up on the jargons)*/
+
+
+        /*public static double CalculateFare(double distance)
         {
             //YOUR CODE HERE
             return 0;
 
-        }
+        }*/
     }
 }
